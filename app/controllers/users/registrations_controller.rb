a27e8f750bc1c1a881eecfc4672a8ad248fc1416
Matsu <matsu@shin.local>
2022-06-25 00:19:18 +0900

    created signup,login function

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,19 +5,20 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  def new
-    @user = User.new
-  end
+  # def new
+  #   @user = User.new
+  # end
 
   # POST /resource
-  def create
-    @user = User.new(params.require(:user).permit(:name, :email, :password, :password_confirmation))
-    if @user.save
-      flash[:notice] = "Created new account"
-    else
-      render "new"
-    end
-  end
+  # def create
+  #   @user = User.new(params.require(:user).permit(:name, :email, :password, :password_confirmation))
+  #   if @user.save
+  #     flash[:notice] = "Created new account"
+  #     redirect_to pages_show_path
+  #   else
+  #     render "new"
+  #   end
+  # end
 
   # GET /resource/edit
   # def edit
