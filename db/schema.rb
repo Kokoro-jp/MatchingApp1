2a5d8450a723b746db6bdf79e3cfbf426a20dffc
Matsu <matsu@shin.local>
2022-07-03 18:24:59 +0900

    created reservation model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_28_093227) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_03_074243) do
+  create_table "reservations", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "person_num"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "rooms", force: :cascade do |t|
     t.string "room_name"
     t.string "room_introduction"
@@ -19,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_28_093227) do
     t.string "room_img"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
